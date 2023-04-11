a2509eb75d885da26efa21e5428232a1c316f1ea
Pavel_Artemyev <pasha.artemev48@gmail.com>
2023-04-11 11:32:16 +0300

    Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 22, 36, 98, 59, 67, 73, 85, 98 };
+
+int number = array.Length;
+int find = 98;
+
+int index = 0;
+
+while (index < number)
+{
+    if(array[index] == find)
+    {
+     Console.WriteLine(index);
+     break;
+    }
+    index++;
+}
