800769bb70e415c5aca04850f95e9395dd1f6e95
Vado <vado@MBP-Paul.Dlink>
2023-04-24 22:04:33 +0300

    Examole 014 completed

---
+++ Example014_Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿// Создаем функцию двумерного массива
+
+int [,] matrix = new int [3,3];
+
+void PrintArray (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+// Заполняем нашу функцию случайными числами
+void FillArray (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+FillArray(matrix);
+PrintArray(matrix);
